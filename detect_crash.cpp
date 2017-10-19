1c535aeb02f8a9d6da54169104a463a6ac769073
Hasan Sait <hasan.sait.arslan@gmail.com>
2017-10-19 14:53:02 +0300

    PART5

--- detect_crash.cpp
+++ detect_crash.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 #include <string>
+#include <opencv2/nonfree/features2d.hpp>
 
 using namespace std;
 using namespace boost::program_options;
@@ -13,12 +14,11 @@ using namespace cv;
 
 namespace fs = boost::filesystem;
 
-int	frames2skip, frameDiffGaussMask, damageDetectMaskSize, frameDmgDialMask;
+int	frames2skip, frameDiffGaussMask, damageDetectMaskSize, frameDmgDialMask, surfHessianThs, minNrOfTrackedFeatures, minNrOfAcceptedFeatures, featurePointDistClusterThs,
+	ransacProjThreshold, ransacProjThs_1_to, try2MergeXclusters, try2MergeBeforeAfterSteps, try2MergeChangeBase;
 
-float	frameDiffAvg, frameDiffThs, windowHorizontal, windowVertical, damageSensitivity, verificationLevels, verifDamageMaskDilate, surfHessianThs, featurePointDistThs, 
-	minNrOfTrackedFeatures, minNrOfAcceptedFeatures, featurePointDistClusterThs, featurePointAngeleMedian, featurePointAngeleThs, ransacProjThreshold, ransacProjThs_1_to, 
-	try2MergeXclusters, try2MergeBeforeAfterSteps, try2MergeChangeBase, displayFrameDifference, displayDamages, displayAllFeatures, displayFeatures, 
-	displayVerifiedDamages, exitWhenDone;
+float	frameDiffAvg, frameDiffThs, windowHorizontal, windowVertical, damageSensitivity, verificationLevels, verifDamageMaskDilate, featurePointDistThs, 
+	featurePointAngeleMedian, featurePointAngeleThs, displayFrameDifference, displayDamages, displayAllFeatures, displayFeatures, displayVerifiedDamages, exitWhenDone;
 
 void extract_videos(vector<boost::filesystem::path>& videos, fs::path full_path)
 {
@@ -100,8 +100,90 @@ void find_damage_mask(Mat currentFrame, int frame_height, int frame_width, int m
 		stop:;
 	}
 
-	cout << dmgSum << endl;
+	//cout << dmgSum << endl;
 	if (frameDmgDialMask > 0) dilate(dmgMaskDamage, dmgMaskDamage, frameDmgDialMaskElement);
+}
+
+void find_the_features(Mat previousFrame, int minRow, int maxRow, int minCol, int maxCol, Mat& Tform, Mat& InvTform, Mat currentFrame, float maxDiststance, 
+			vector<DMatch>& matches)
+{
+	// previous frame
+	Mat img_objectRGB1 = previousFrame.clone();
+	Mat img_objectHSV1;
+	cvtColor(img_objectRGB1, img_objectHSV1, CV_RGB2HSV);
+	vector<Mat>chHSVout(3);
+	split(img_objectHSV1, chHSVout);
+	Mat img_frame1 = chHSVout[2].clone();
+	equalizeHist(img_frame1, img_frame1);
+	FastFeatureDetector detector(20);
+	//SurfFeatureDetector detector(surfHessianThs, 2, 2);
+	vector<KeyPoint> keypoints_frame1, keypoints_frame11;
+	detector.detect(img_frame1, keypoints_frame1);
+
+	int length = keypoints_frame1.size();
+	for (int num = 0; num < length; num++)
+	{
+		Point2f temp = keypoints_frame1[num].pt;
+		if (temp.y > minRow && temp.y < maxRow)
+			if (temp.x > minCol && temp.x < maxCol)
+				keypoints_frame11.push_back(keypoints_frame1[num]);
+	}
+
+	SurfDescriptorExtractor extractor;
+	Mat descriptors_frame1;
+	extractor.compute(img_frame1, keypoints_frame11, descriptors_frame1);
+	Tform = Mat::eye(3, 3, CV_64F);
+	InvTform = Mat::eye(3, 3, CV_64F);
+	// current frame
+	Mat img_objectRGB2 = currentFrame.clone();
+	Mat img_objectHSV2;
+	cvtColor(img_objectRGB2, img_objectHSV2, CV_RGB2HSV);
+	vector<Mat> chHSVout2(3);
+	split(img_objectHSV2, chHSVout2);
+	Mat img_frame2 = chHSVout2[2].clone();
+	equalizeHist(img_frame2, img_frame2);
+	vector<KeyPoint> keypoints_frame2, keypoints_frame22;
+	detector.detect(img_frame2, keypoints_frame2);
+
+	length = keypoints_frame2.size();
+	for (int num = 0; num < length; num++)
+	{
+		Point2f temp = keypoints_frame2[num].pt;
+		if (temp.y > minRow && temp.y < maxRow)
+			if (temp.x > minCol && temp.x < maxCol)
+				keypoints_frame22.push_back(keypoints_frame2[num]);
+	}
+
+	Mat descriptors_frame2;
+	extractor.compute(img_frame2, keypoints_frame22, descriptors_frame2);
+
+	// match the features
+	FlannBasedMatcher matcher;
+	vector<DMatch> matchesFrame12;
+	matcher.match(descriptors_frame1, descriptors_frame2, matchesFrame12);
+
+	length = matchesFrame12.size();
+	float dist;
+	vector<float> trueDistRaw;
+	vector<float> trueDistSortedRaw;
+	vector<float> vectorDist;
+	vector<Point2f> frame1_obj;
+	vector<Point2f> frame2_scene;
+	for (int num = 0; num < length; num++)
+	{
+		Point2f P_end = keypoints_frame11[matchesFrame12[num].queryIdx].pt;
+		Point2f P_start = keypoints_frame22[matchesFrame12[num].trainIdx].pt;
+		dist = sqrt(pow((P_end.x - P_start.x), 2) + pow((P_end.y - P_start.y), 2));
+		if (dist < maxDiststance)
+		{
+			trueDistRaw.push_back(dist);
+			trueDistSortedRaw.push_back(dist);
+			matches.push_back(matchesFrame12[num]);
+			vectorDist.push_back(matchesFrame12[num].distance);
+			frame1_obj.push_back(keypoints_frame11[matchesFrame12[num].queryIdx].pt);
+			frame2_scene.push_back(keypoints_frame22[matchesFrame12[num].trainIdx].pt);
+		}
+	}
 }
 
 void process_video(cv::VideoCapture capVideo, fs::path full_path, fs::path videoName, string timestamp)
@@ -172,6 +254,14 @@ void process_video(cv::VideoCapture capVideo, fs::path full_path, fs::path video
 	Mat frameDmgDialMaskElement = getStructuringElement(MORPH_ELLIPSE, Size(2 * frameDmgDialMask + 1, 2 * frameDmgDialMask + 1), 
 					Point(frameDmgDialMask, frameDmgDialMask));
 
+	// find_the_features VALUES
+	int transCounter;
+	float maxDiststance;
+	if (frame_height >= frame_width) maxDiststance = frame_height * featurePointDistThs;
+	else maxDiststance = frame_width * featurePointDistThs;
+	double angleBinVal[360] = {};
+	for (int i = 360, j = 0; i > 0; i--, j++) angleBinVal[j] = float(i - 180.0);
+
 	while (noFrame < numberOfFrames)
 	{
 		//cout << noFrame << endl;
@@ -186,7 +276,10 @@ void process_video(cv::VideoCapture capVideo, fs::path full_path, fs::path video
 			Mat dmgMaskDamage(frame_height, frame_width, CV_8U, Scalar::all(0));
 			find_damage_mask(currentFrame, frame_height, frame_width, maxCol, minCol, CD1, CD2, minRow, maxRow, 
 						frameDmgDialMaskElement, dmgMaskDamage, dmgMaskSum);
-			capVideoGood.write(currentFrame);
+			Mat Tform, InvTform;
+			vector<DMatch> matches;
+			find_the_features(previousFrame, minRow, maxRow, minCol, maxCol, Tform, InvTform, currentFrame, maxDiststance, matches);
+			//capVideoGood.write(currentFrame);
 		}
 		Mat previousFrame;
 		previousFrame = currentFrame.clone();
@@ -243,18 +336,18 @@ int main(int argc, char* argv[])
 	("damageDetectMaskSize", value<int>(&damageDetectMaskSize)->default_value(5))
 	("verificationLevels", value<float>(&verificationLevels)->default_value(3.0))
 	("verifDamageMaskDilate", value<float>(&verifDamageMaskDilate)->default_value(1.0))
-	("surfHessianThs", value<int>(&surfHessianThs)->default_value(100.0))
+	("surfHessianThs", value<int>(&surfHessianThs)->default_value(100))
 	("featurePointDistThs", value<float>(&featurePointDistThs)->default_value(0.25))
-	("minNrOfTrackedFeatures", value<float>(&minNrOfTrackedFeatures)->default_value(5.0))
-	("minNrOfAcceptedFeatures", value<float>(&minNrOfAcceptedFeatures)->default_value(10.0))
-	("featurePointDistClusterThs", value<float>(&featurePointDistClusterThs)->default_value(0.0))
+	("minNrOfTrackedFeatures", value<int>(&minNrOfTrackedFeatures)->default_value(5))
+	("minNrOfAcceptedFeatures", value<int>(&minNrOfAcceptedFeatures)->default_value(10))
+	("featurePointDistClusterThs", value<int>(&featurePointDistClusterThs)->default_value(0))
 	("featurePointAngeleMedian", value<float>(&featurePointAngeleMedian)->default_value(5.0))
 	("featurePointAngeleThs", value<float>(&featurePointAngeleThs)->default_value(20.0))
-	("ransacProjThreshold", value<float>(&ransacProjThreshold)->default_value(5.0))
-	("ransacProjThs_1_to", value<float>(&ransacProjThs_1_to)->default_value(15.0))
-	("try2MergeXclusters", value<float>(&try2MergeXclusters)->default_value(5.0))
-	("try2MergeBeforeAfterSteps", value<float>(&try2MergeBeforeAfterSteps)->default_value(7.0))
-	("try2MergeChangeBase", value<float>(&try2MergeChangeBase)->default_value(5.0))
+	("ransacProjThreshold", value<int>(&ransacProjThreshold)->default_value(5))
+	("ransacProjThs_1_to", value<int>(&ransacProjThs_1_to)->default_value(15))
+	("try2MergeXclusters", value<int>(&try2MergeXclusters)->default_value(5))
+	("try2MergeBeforeAfterSteps", value<int>(&try2MergeBeforeAfterSteps)->default_value(7))
+	("try2MergeChangeBase", value<int>(&try2MergeChangeBase)->default_value(5))
 	("displayFrameDifference", value<float>(&displayFrameDifference)->default_value(0.0))
 	("displayDamages", value<float>(&displayDamages)->default_value(0.0))
 	("displayAllFeatures", value<float>(&displayAllFeatures)->default_value(0.0))
@@ -273,6 +366,18 @@ int main(int argc, char* argv[])
         damageSensitivity = opts["damageSensitivity"].as<float>();
 	damageDetectMaskSize = opts["damageDetectMaskSize"].as<int>();
 	frameDmgDialMask = opts["frameDmgDialMask"].as<int>();
+	surfHessianThs = opts["surfHessianThs"].as<int>();
+	featurePointDistThs = opts["featurePointDistThs"].as<float>();
+	minNrOfTrackedFeatures = opts["minNrOfTrackedFeatures"].as<int>();
+	minNrOfAcceptedFeatures = opts["minNrOfAcceptedFeatures"].as<int>();
+	featurePointDistClusterThs = opts["featurePointDistClusterThs"].as<int>();
+	featurePointAngeleMedian = opts["featurePointAngeleMedian"].as<float>();
+	featurePointAngeleThs = opts["featurePointAngeleThs"].as<float>();
+	ransacProjThreshold = opts["ransacProjThreshold"].as<int>();
+	ransacProjThs_1_to = opts["ransacProjThs_1_to"].as<int>();
+	try2MergeXclusters = opts["try2MergeXclusters"].as<int>();
+	try2MergeBeforeAfterSteps = opts["try2MergeBeforeAfterSteps"].as<int>();
+	try2MergeChangeBase = opts["try2MergeChangeBase"].as<int>();
 
 	vector<boost::filesystem::path> videos;
 	fs::path full_path(fs::current_path() / "videos");
